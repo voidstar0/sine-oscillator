b5fda31ba187dddeea71254fe4aca837b2cfc88a
char <25884226+char@users.noreply.github.com>
2019-01-03 05:44:50 -0500

    Refactor

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import scipy.io.wavfile
 fs = 44100; #hz
 freq = 1000;
 wave = 2*math.pi*freq * np.array(list(range(1, fs + 1))) / fs
+tones = np.asarray([math.sin(x) for x in wave])
 
-tones = [math.sin(x) for x in wave]
-
-tones = np.asarray(tones)
 scipy.io.wavfile.write('test.wav',fs,tones)
